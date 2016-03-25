0456612f5e4a87c0a91e4a9de895804f0606e63e
Akash Patel <akashnpatel009@gmail.com>
2016-03-24 20:35:20 -0700

    Added Images/Buttons/Animations

--- app/src/main/java/com/example/akash/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/akash/bucketdrops/ActivityMain.java
@@ -2,12 +2,42 @@ package com.example.akash.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private Toolbar mToolBar;
+    private Button mBtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+
+        initBackGroundImage();
+
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initBackGroundImage(){
+        ImageView backGround = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(backGround);
     }
 }
